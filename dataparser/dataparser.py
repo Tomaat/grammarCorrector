a7e4ac4a717e0ae4b9ecb59e45d29eca60cff8c1
MBleeker <maurits.bleeker@nubisonline.nl>
2015-11-13 21:18:59 +0100

    documented the code a bit, made some small modifications

--- dataparser/dataparser.py
+++ dataparser/dataparser.py
@@ -20,6 +20,10 @@ class Sentence:
 		# return words without the punctuatuin 
 		return self.sentence_words
 
+	def getErrorList():
+		# return a list with array objects for that sentence 
+		return self.error_list
+
 	def setSentenceErros(self, sentance_tuple):
 		error_list = []
 		for error_tuple in sentance_tuple[1]:
@@ -29,11 +33,11 @@ class Sentence:
 
 class Mistake:
 	def __init__(self, error_tuple):
-		self.error_type = error_tuple[1]
-		self.correction = error_tuple[2]
-		self.error_start_index = error_tuple[0].split(' ')[1] 
-		self.error_end_index = error_tuple[0].split(' ')[2] 
-		self.error_word =  self.setErrorWord(error_tuple) 
+		self.error_type = error_tuple[1] # type of the grammer mistake 
+		self.correction = error_tuple[2]	# the correction of the mistake
+		self.error_start_index = error_tuple[0].split(' ')[1]  	# start index in the sentence for where the mistake has been made
+		self.error_end_index = error_tuple[0].split(' ')[2] 	# end index of the mistake 
+		self.error_word =  self.setErrorWord(error_tuple) 		# the word, of the sentence part that is wrong
 		self.error_hash = error_has = {
 								"Vt":"Verb tense",
 								"Vm":"Verb modal",
@@ -61,18 +65,20 @@ class Mistake:
 								"Rloc":"Local redundancy",
 								"Cit":"Citation",
 								"Others":"Other errors",
-								"Um":"Unclear meaning (cannot be corrected)",
-							}
+								"Um":"Unclear meaning (cannot be corrected)",						# hash with the full discription of every mistake in the dataset 
+		}
 
-	def giveFullMistakeDeclaration(self):			
+	def giveFullMistakeDeclaration(self):
+		# returns the the full description of the mistake 			
 		return self.error_has[self.error_type]
 
 	def setErrorWord(self,error_tuple):
+		# returns the word where the mistake has been made, is the word is empty, a word has been forgotten or another  mistake has been made
 		splited_sentane = error_tuple[0].split(' ')
 		try:
 			return splited_sentane[int(self.error_start_index):int (self.error_end_index)]
 		except ValueError:
-			print error_tuple[0]
+			print "Error word can not be set, error tuple:"+error_tuple[0]
 
 if __name__ == '__main__':
 	with open ('../release3.2/data/conll14st-preprocessed.m2') as datafile: # import sgml data-file
