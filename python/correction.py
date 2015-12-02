82f33d9ef3fe2f878dd37f4622672d0c198f79c5
maartjeth <maartjeterhoeve@gmail.com>
2015-12-02 21:56:50 +0100

    error corrector for left to right, scores might be tweaked a bit

--- python/correction.py
+++ python/correction.py
@@ -3,6 +3,7 @@ from nltk.corpus import gutenberg, brown
 from nltk import ConditionalFreqDist
 from nltk import ngrams
 from math import log10
+import spacy
 
 from random import choice
 
@@ -20,6 +21,11 @@ class NGrams:
 
 			Find all the n-grams in the brown corpus. Store in frequency dictionary.
 			Optionally it can be decided to use more corpora in order to have more data.
+
+			Note: these are of course n-grams based on going through the sentence from left to right
+			If we want to give the correction back based on the dependency tree, we need to
+			parse the brown corpus (or any other data set) with the dependency parser, so that
+			we can use this data. 			
 
 		"""
 
@@ -70,16 +76,23 @@ class Correction(NGrams):
 	def __init__(self):
 		self.pot_corrections = []
 		self.ngram_corrections = []
+		self.final_corrections = []
+		self.final_sorted_list = []
 
 	def find_correction(self, ngram_dict, error_sequence, error_tag):
 		""" Input:	the n-gram that ends in the error
 					the error tag predicted by the structured perceptron
 
-			Output:	corrected word
+			Output:	corrected word --> in the code around this, this needs to be added to the new sentence
 
 			Function to find a correction for the mistake
 			1) What words do we expect based on our n-gram dictionary?
-			2) What do we expected based on (1) and the word distance (spacy)?
+			2) What do we expect based on (1) and the word distance (spacy)? -->
+			   This only works if the error was actually a well written word, otherwise we need
+			   to use something else
+			   Plus: spacy uses word similarity based on semantics. We rather want it on word form
+
+			3) Potentially: take the error_tag into account --> 
 		"""
 
 		len_sequence = len(error_sequence)-1
@@ -87,29 +100,54 @@ class Correction(NGrams):
 		# 1) Find suitable n-grams
 		for key,value in ngram_dict.iteritems():
 			if key[0:len_sequence] == error_sequence[0:len_sequence]:
-				self.pot_corrections.append((key[len_sequence], value))
 
-		self.pot_corrections.append(("u'testword", -2))
-		self.pot_corrections.append(("u'testword2", -8))
+				# You want to give a higher score to words that have many letters in common --> might want to take word length into account
+				score = 0
+				no_letters = 0
+				error = error_sequence[len_sequence]
+				for char in key[len_sequence]:
+					no_letters += 1
+					if char in error:
+						score += 1
+
+				score = float(score)/no_letters
+
+				final_score = -1*value*(2**score) # this is something you can refine
+				print 'Value: ', value
+				print 'Score: ', score
+				print 'Final score: ', final_score
+
+				self.pot_corrections.append((key[len_sequence], final_score))
+
+		# For testing
+		self.pot_corrections.append((u'jus', 2))
+		self.pot_corrections.append((u'test', 8))
 
 		sortedlist = sorted(self.pot_corrections, key=lambda x:x[1])
 
 		# 1b) Find the best n-grams. 
 		no_best_ngrams = 2
-		self.ngram_corrections = sortedlist[-no_best_ngrams:]
+		self.ngram_corrections = sortedlist[no_best_ngrams-1:] # note this is from small to big
+		print self.ngram_corrections
 
+		# 2) Select n-grams based on word distance (spacy) --> you might not want to do this --> lot of effort
+		print "Getting all the English spacy stuff..."
+		nlp = spacy.en.English()
+		print "Done with that!"
 
-		# 2) Select n-grams based on word distance (spacy)
+		spacy_error = nlp(unicode(error_sequence[len_sequence], encoding="utf-8"))
 
+		print "Finding similarity measures"
+		for correction in self.ngram_corrections:
+			spacy_correction = nlp(correction[0]) # assuming that this is already written in unicode
+			similarity_score = spacy_correction.similarity(spacy_error)
+			word_score = correction[1]*(2**similarity_score)
+			self.final_corrections.append((correction[0], word_score))
 
-			
+		self.final_sorted_list = sorted(self.final_corrections, key=lambda x:x[1])
+		print self.final_sorted_list
 
-
-
-
-
-
-
+		return self.final_sorted_list # now you can try the proposed corrections
 
 
 
@@ -123,5 +161,6 @@ if __name__ == '__main__':
 	#print ngram_dict
 
 	c = Correction()
+	from spacy.en import LOCAL_DATA_DIR, English
 	print "Finding corrections..."
-	c.find_correction(ngram_dict, ("will", "behave", "just"), 'true')
+	c.find_correction(ngram_dict, ("will", "behave", "jist"), 'true')
